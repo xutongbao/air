330bf5f0b615dd575afe882e6201cbe03e9b26dd
xutongbao <1183391880@qq.com>
2021-08-20 10:22:24 +0800

    1

--- src/views/light/admin/setApplicationPlus/Dustbin.js
+++ src/views/light/admin/setApplicationPlus/Dustbin.js
@@ -1,37 +1,18 @@
-import { useDrop } from 'react-dnd';
-import { ItemTypes } from './ItemTypes';
-const style = {
-    height: '12rem',
-    width: '12rem',
-    marginRight: '1.5rem',
-    marginBottom: '1.5rem',
-    color: 'white',
-    padding: '1rem',
-    textAlign: 'center',
-    fontSize: '1rem',
-    lineHeight: 'normal',
-    float: 'left',
-};
+import { useDrop } from 'react-dnd'
+import { ItemTypes } from './ItemTypes'
 export const Dustbin = () => {
-    const [{ canDrop, isOver }, drop] = useDrop(() => ({
-        accept: ItemTypes.BOX,
-        drop: () => ({ name: 'Dustbin' }),
-        collect: (monitor) => ({
-            isOver: monitor.isOver(),
-            canDrop: monitor.canDrop(),
-        }),
-    }));
-    const isActive = canDrop && isOver;
-    let backgroundColor = '#222';
-    if (isActive) {
-        backgroundColor = 'darkgreen';
-    }
-    else if (canDrop) {
-        backgroundColor = 'darkkhaki';
-    }
-    return (<div ref={drop} 
-    //role={'Dustbin'} 
-    style={{ ...style, backgroundColor }}>
-			{isActive ? 'Release to drop' : 'Drag a box here'}
-		</div>);
-};
+  const [{ canDrop, isOver }, drop] = useDrop(() => ({
+    accept: ItemTypes.BOX,
+    drop: () => ({ name: 'Dustbin' }),
+    collect: (monitor) => ({
+      isOver: monitor.isOver(),
+      canDrop: monitor.canDrop(),
+    }),
+  }))
+  const isActive = canDrop && isOver
+  return (
+    <div ref={drop} className={`m-center-list-wrap ${isActive ? 'active' : ''}`}>
+      
+    </div>
+  )
+}
