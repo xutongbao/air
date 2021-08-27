c05da74ed1fad9cd9222d897c25b9e9e9b36cef9
xutongbao <1183391880@qq.com>
2021-08-27 10:11:37 +0800

    1

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,9 +1,9 @@
-let baseURL = 'http://localhost:81'
-if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:81' //"http://localhost:81"
-} else {
-  baseURL = 'https://efficacious-tiny-infinity.glitch.me'
-  //baseURL = 'http://39.97.238.175:81'
-}
+const baseURL = {
+  // 使用反向代理解决跨域时，dev应为空字符串
+  dev: 'http://localhost:81',
+  test: 'https://efficacious-tiny-infinity.glitch.me',
+  // 使用yarn build打包，所有接口使用这个url，需要服务器解决跨域
+  prod: 'http://39.97.238.175:81'
+}[process.env.REACT_APP_MODE]
 
 export { baseURL }
