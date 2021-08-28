7f53dca762aef38130ba922c2bca91bf286d6dda
xutongbao <1183391880@qq.com>
2021-08-28 18:30:30 +0800

    1

--- src/views/light/formview/useList.js
+++ src/views/light/formview/useList.js
@@ -33,7 +33,7 @@ export default function useList(props) {
         const fields = res.data.fields
         setModalFields(getModalFields(res.data.fields))
         setTitle(res.data.title)
-        setSkin(res.data.skin)
+        setSkin(res.data.skin ? res.data.skin : {})
         const tempFields = fields.filter((item) => item.isModalField)
         if (Array.isArray(tempFields) && tempFields.length > 0) {
           if (tempFields[0].type === 'image') {
