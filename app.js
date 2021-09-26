5b22fa28f08459dbfe54f0f23c1e7932a41d1f13
xutongbao <1183391880@qq.com>
2021-09-26 14:57:59 +0800

    1

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express')
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
 const app = express()
 app.use(express.static('build'))
+
+//通过代理解决跨域
+app.use(
+  '/api',
+  createProxyMiddleware({
+    target: 'http://localhost:81',
+    changeOrigin: true,
+  })
+)
+
 app.listen(process.env.PORT, () => {
   console.log(process.env.PORT)
 })
