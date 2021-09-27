0b353c84d7b33d66d83ac55b2a01dfa5d69e2102
xutongbao <1183391880@qq.com>
2021-09-27 11:07:28 +0800

    1

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const { createProxyMiddleware } = require('http-proxy-middleware')
+const compression = require('compression');
 
 const app = express()
+app.use(compression({ filter: shouldCompress }))
 app.use(express.static('build'))
+function shouldCompress(req, res) {
+  if (req.headers['x-no-compression']) {
+    // don't compress responses with this request header
+    return false;
+  }
+
+  // fallback to standard filter function
+  return compression.filter(req, res);
+}
 
 //通过代理解决跨域
 app.use(
