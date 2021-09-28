fec5675fa5baad3e67c591ce93cd29d13a67456d
xutongbao <1183391880@qq.com>
2021-09-28 16:23:28 +0800

    1

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@ call yarn
 echo install done
 set MODE_ENV=jenkins 
 set BUILD_PATH=build
-set PUBLIC_URL=/air/%branch% 
+set PUBLIC_URL=/air/%branch%
 set GENERATE_SOURCEMAP=false 
 call node scripts/build.js
 xcopy D:\Jenkins\workspace\air\build D:\temp\air\%branch:/=\%\ /Y /E
