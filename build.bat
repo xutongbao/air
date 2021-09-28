9eb2b3ed808a43e21e119088805ce58b17439029
xutongbao <1183391880@qq.com>
2021-09-28 15:15:34 +0800

    1

--- build.bat
+++ build.bat
@@ -7,6 +7,7 @@ set aa=%aa:world=china%
 echo aa=%aa% 
 set branchTemp=%branch:/=\%
 echo %branchTemp%
-yarn
+yarn install
+echo install done
 cross-env MODE_ENV=jenkins BUILD_PATH=/temp/air/%branch% PUBLIC_URL=/air/%branch% GENERATE_SOURCEMAP=false node scripts/build.js
 @REM xcopy D:\\Jenkins\\workspace\\air D:\\temp\\air\\%branchTemp%\\ /Y /E
