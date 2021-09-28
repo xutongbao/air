884c2566b4b5614fd12a112b58bcdda5a988386e
xutongbao <1183391880@qq.com>
2021-09-28 16:15:16 +0800

    1

--- build.bat
+++ build.bat
@@ -1,17 +1,8 @@
-set aa=hello,world!
-echo before:%aa%
-echo after:%aa:world=china%
-echo aa=%aa%
-echo %aa%
-set aa=%aa:world=china%
-echo aa=%aa% 
-set branchTemp=%branch:/=\%
-echo %branchTemp%
 call yarn
 echo install done
 set MODE_ENV=jenkins 
-set BUILD_PATH=/temp/air/%branch% 
+set BUILD_PATH=build
 set PUBLIC_URL=/air/%branch% 
 set GENERATE_SOURCEMAP=false 
-node scripts/build.js
-@REM xcopy D:\\Jenkins\\workspace\\air D:\\temp\\air\\%branchTemp%\\ /Y /E
+call node scripts/build.js
+xcopy D:/Jenkins/workspace/air/build D:/temp/air/%branch% /Y /E
