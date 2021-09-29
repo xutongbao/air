4c403dcd56271d1a2223f47587617848523e7be1
xutongbao <1183391880@qq.com>
2021-09-29 09:55:23 +0800

    1

--- build.bat
+++ build.bat
@@ -5,6 +5,6 @@ set BUILD_PATH=build
 set PUBLIC_URL=/air/%branch%
 set GENERATE_SOURCEMAP=false 
 call node scripts/build.js
-xcopy D:\Jenkins\workspace\air\build D:\temp\air\%branch:/=\%\ /Y /E
+xcopy \Jenkins\workspace\air\build \temp\air\%branch:/=\%\ /Y /E
 call set myBranch=/air/%branch% && node buildDone
 echo http://localhost:81/air/%branch%
