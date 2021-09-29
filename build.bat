730e849ed98254f8c5ebf58452869d06650ae897
xutongbao <1183391880@qq.com>
2021-09-29 13:59:23 +0800

    1

--- build.bat
+++ build.bat
@@ -6,5 +6,9 @@ set PUBLIC_URL=/air/%branch%
 set GENERATE_SOURCEMAP=false 
 call node scripts/build.js
 xcopy \Jenkins\workspace\air\build \temp\air\%branch:/=\%\ /Y /E
-call set myBranch=/air/%branch% && node buildDone
+for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
+ set IP=%%a
+)
+echo %IP%
+call set myBranch=/air/%branch% IP=%IP% && node buildDone
 echo http://39.97.238.175:81/air/%branch%
--- buildDone.js
+++ buildDone.js
@@ -1,26 +1,56 @@
 const axios = require('axios')
 
-const emailPost = (emailData) => {
+const baseURL = `http://${process.env.IP}:81${process.env.myBranch}`
+console.log(baseURL)
+
+const email = () => {
+  const emailData = {
+    type: 'jenkins',
+    title: '构建成功',
+    username: 'xxx',
+    path: `${baseURL}{process.env.myBranch}`,
+    errorTitle: '构建',
+    detail: '构建成功',
+    browser: '1',
+  }
   axios
-    .post('http://39.97.238.175:81/api/log/email', {
+    .post(`${baseURL}/api/log/email`, {
       ...emailData,
     })
-    .then((res) => {})
+    .then((res) => {
+      console.log('发送邮件成功！')
+    })
     .catch((error) => {
       console.error(error)
     })
 }
 
-const email = () => {
-  const tempValues = {
-    type: 'jenkins',
-    title: '构建成功',
-    username: 'xxx',
-    path: `http://39.97.238.175:81${process.env.myBranch}`,
-    errorTitle: '构建',
-    detail: '构建成功',
-    browser: '1',
+const handleAddRecord = () => {
+  const dataItem = {
+    id: Date.now(),
+    name: '无代码平台',
+    gitRepositorieName: 'air',
+    branch: 'origin/master',
+    url: '/air/origin/master/#/light/index/content?id=0',
+    remarks: '自动'
   }
-  emailPost(tempValues)
+  axios
+    .post(`${baseURL}/api/jenkins/add`, {
+      dataItem,
+    })
+    .then((res) => {
+      console.log('添加记录成功！')
+    })
+    .catch((error) => {
+      console.error(error)
+    })
+  Api.light.jenkinsAdd({ dataItem: values }).then((res) => {
+    if (res.state === 1) {
+      setIsModalVisible(false)
+      handleSearch()
+    }
+  })
 }
+
 email()
+handleAddRecord()
