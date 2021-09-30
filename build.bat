85ad99498ef753b1194cb1287fbc4b289a57ccca
xutongbao <1183391880@qq.com>
2021-09-30 12:46:54 +0800

    优化

--- build.bat
+++ build.bat
@@ -11,7 +11,7 @@ set REACT_APP_MODE=prod
 set BUILD_PATH=build
 set PUBLIC_URL=/%gitRepositorieName%/%branch%
 set GENERATE_SOURCEMAP=false
-call node scripts/build.js
+call node --max_old_space_size=4096 scripts/build.js
 
 @REM 复制编译好的的代码到新目录
 xcopy \Jenkins\workspace\%gitRepositorieName%\build \temp\%gitRepositorieName%\%branch:/=\%\ /Y /E
