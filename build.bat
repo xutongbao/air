b03c2a2e19db6a0aa7286da8376e5b8148f8b042
xutongbao <1183391880@qq.com>
2021-09-30 10:32:50 +0800

    优化

--- build.bat
+++ build.bat
@@ -6,7 +6,8 @@ echo install done
 set gitRepositorieName=air
 
 @REM 设置环境变量并编译
-set MODE_ENV=production 
+set MODE_ENV=production
+set REACT_APP_MODE=prod 
 set BUILD_PATH=build
 set PUBLIC_URL=/%gitRepositorieName%/%branch%
 set GENERATE_SOURCEMAP=false 
@@ -15,14 +16,8 @@ call node scripts/build.js
 @REM 复制编译好的的代码到新目录
 xcopy \Jenkins\workspace\%gitRepositorieName%\build \temp\%gitRepositorieName%\%branch:/=\%\ /Y /E
 
-@REM 查询IP地址
-for /f "tokens=4" %%a in ('route print^|findstr 0.0.0.0.*0.0.0.0') do (
- set IP=%%a
-)
-echo %IP%
-
 @REM 设置环境变量并执行js脚本发送邮件通知、添加构建记录
 set myBranch=/%gitRepositorieName%/%branch%
-set IP=%IP%
+set IP=39.97.238.175
 call node buildDone
 echo http://39.97.238.175:81/%gitRepositorieName%/%branch%
