90245fb6e67175e69ccf64e08cd01a40f9349425
xutongbao <1183391880@qq.com>
2021-10-20 18:19:00 +0800

    优化

--- jenkins/prod/build.bat
+++ jenkins/prod/build.bat
@@ -4,6 +4,7 @@ echo install done
 
 @REM 设置git仓库名称
 set gitRepositorieName=air
+set pipeline=prod
 
 @REM 设置环境变量并编译
 set MODE_ENV=production
@@ -14,7 +15,7 @@ set GENERATE_SOURCEMAP=false
 call node --max_old_space_size=4096 scripts/build.js
 
 @REM 复制编译好的的代码到新目录
-xcopy \Jenkins\workspace\%gitRepositorieName%\build \temp\%gitRepositorieName%\ /Y /E
+xcopy \Jenkins\workspace\%pipeline%-%gitRepositorieName%\build \temp\%gitRepositorieName%\ /Y /E
 
 @REM 设置环境变量并执行js脚本发送邮件通知、添加构建记录
 set computername=%computername%
