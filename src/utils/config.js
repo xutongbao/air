28e4dfb2b9c0239c51b90431c6d44b590e9d1935
xutongbao <1183391880@qq.com>
2021-10-21 11:00:09 +0800

    优化

--- src/utils/config.js
+++ src/utils/config.js
@@ -2,7 +2,7 @@ const baseURL = {
   // 使用反向代理解决跨域时，dev应为空字符串
   dev: 'http://localhost:81',
   //test: 'https://efficacious-tiny-infinity.glitch.me',
-  test: 'http://localhost:81',
+  test: 'http://39.97.238.175:81',
   // 使用yarn build打包，所有接口使用这个url，需要服务器解决跨域
   prod: 'http://39.97.238.175:84'
 }[process.env.REACT_APP_MODE]
@@ -11,7 +11,7 @@ const nodeBaseURL = {
   dev: 'http://localhost:81',
   // test: 'https://efficacious-tiny-infinity.glitch.me',
   // prod: 'https://efficacious-tiny-infinity.glitch.me',
-  test: 'http://localhost:81',
+  test: 'http://39.97.238.175:81',
   prod: 'http://39.97.238.175:84',
 }[process.env.REACT_APP_MODE]
 
