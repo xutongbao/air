cab9a790766708dfb114486980830c3ad133664b
xutongbao <1183391880@qq.com>
2021-11-08 18:05:41 +0800

    优化

---
+++ src/views/light/admin/processNew/detail/Tool.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Button } from 'antd'
+import { Icon } from '../../../../../components/light'
+
+function Tool(props) {
+  const { onAdd, myAuthObj } = props
+
+  return (
+    <>
+      {myAuthObj.add && (
+        <div className="m-content-tool">
+          <div className="m-content-tool-row">
+            <Button className="m-space" type="primary" onClick={onAdd}>
+              <Icon name="add" className="m-tool-btn-icon"></Icon>
+              添加分类
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+export default Tool
---
+++ src/views/light/admin/processNew/detail/config.js
@@ -0,0 +1,109 @@
+import { Form, Input, Button, TreeSelect } from 'antd'
+const { TextArea } = Input
+
+//表格列字段
+const getColumns = (props) => {
+  return [
+    {
+      title: '分类名称',
+      dataIndex: 'name',
+    },
+    {
+      title: 'ID',
+      dataIndex: 'id',
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      width: 100,
+      render: (text) => {
+        return (
+          <span
+            className={`m-check-status ${text === 3 ? 'open' : 'forbidden'}`}
+          >
+            {text === 3 ? '已开启' : '已禁用'}
+          </span>
+        )
+      },
+    },
+    {
+      title: '操作',
+      width: 320,
+      render: (record) => {
+        return (
+          <div className="m-action">
+            <Button
+              className={`m-action-btn ${
+                record.status === 3 ? 'forbidden' : 'open'
+              }`}
+              size="small"
+              onClick={() => props.onStatus(record)}
+            >
+              {record.status === 3 ? '禁用' : '开启'}
+            </Button>
+            <Button
+              className="m-action-btn"
+              size="small"
+              onClick={() => props.onAddChild(record)}
+            >
+              新增子类
+            </Button>
+            <Button
+              className="m-action-btn"
+              size="small"
+              onClick={() => props.onEdit(record)}
+            >
+              编辑
+            </Button>
+          </div>
+        )
+      },
+    },
+  ]
+}
+
+//添加编辑查看对话框表单字段
+const getModalFields = ({ treeData }) => {
+  return (
+    <>
+      <Form.Item
+        label="分类名称"
+        name="name"
+        rules={[
+          {
+            required: true,
+            message: '请输入分类名称！',
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+      <Form.Item label="首字母" name="letter">
+        <Input />
+      </Form.Item>
+      <Form.Item
+        label="所属分类"
+        name="parentid"
+        rules={[
+          {
+            required: true,
+            message: '请选择所属分类！',
+          },
+        ]}
+      >
+        <TreeSelect
+          treeData={treeData}
+          getPopupContainer={() => document.getElementById('m-modal-form-info')}
+        ></TreeSelect>
+      </Form.Item>
+      <Form.Item label="备注" name="remark">
+        <TextArea />
+      </Form.Item>
+      <Form.Item label="排序值" name="order">
+        <Input />
+      </Form.Item>
+    </>
+  )
+}
+
+export { getColumns, getModalFields }
