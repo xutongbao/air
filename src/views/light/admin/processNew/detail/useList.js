2e6556ba2bb02733c1895bc985d463f0b5fc0bc3
xutongbao <1183391880@qq.com>
2021-11-11 12:28:43 +0800

    优化

--- src/views/light/admin/processNew/detail/useList.js
+++ src/views/light/admin/processNew/detail/useList.js
@@ -57,8 +57,8 @@ export default function useList(props) {
     confirm({
       title: '该分类及子分类都会被删除，确认要删除吗？',
       onOk() {
-        Api.light.testCategroyDelete({ id: record.id }).then((res) => {
-          if (res.state === 1) {
+        Api.light.processFieldsDelete({ tableId, id: record.id }).then((res) => {
+          if (res.code === 200) {
             handleSearch()
           }
         })
