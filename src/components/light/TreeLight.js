34c0b0d91505ffa340311ae732588b2055e56376
xutongbao <1183391880@qq.com>
2021-11-13 14:40:33 +0800

    优化

--- src/components/light/TreeLight.js
+++ src/components/light/TreeLight.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import TreeCard from './TreeCard'
 import { deepClone } from '../../utils/tools'
-import { treeData1, treeData2, treeData3 } from './data'
+import { treeData1, treeData2, treeData3, treeData4 } from './data'
 
 export default function TreeLight() {
   //如何添加position
@@ -19,13 +19,26 @@ export default function TreeLight() {
         if (Array.isArray(arr[i].children) && arr[i].children.length > 0) {
           let tempChildColIndexArr = []
           let childrenLength = arr[i].children.length
-          if (childrenLength === 1) {
-            tempChildColIndexArr = [childColIndexArr[i]]
-          } else if (arr[i].children.length === 2) {
-            tempChildColIndexArr = [childColIndexArr[i] - 1, childColIndexArr[i] + 1]
-          } else if (arr[i].children.length === 3) {
-            tempChildColIndexArr = [childColIndexArr[i] - 2, childColIndexArr[i], childColIndexArr[i] + 2]
+          //奇数偶数处理方式不同
+          let startColIndex
+          if (childrenLength % 2 === 1) {
+            startColIndex = childColIndexArr[i] - ((childrenLength - 1) / 2) * 2
+          } else {
+            startColIndex = childColIndexArr[i] - ((childrenLength - 2) / 2) * 2 -1
           }
+          for (let i = 0; i < childrenLength; i++) {
+            tempChildColIndexArr.push(startColIndex + i * 2)
+          }
+
+          // if (childrenLength === 1) {
+          //   tempChildColIndexArr = [childColIndexArr[i]]
+          // } else if (arr[i].children.length === 2) {
+          //   tempChildColIndexArr = [childColIndexArr[i] - 1, childColIndexArr[i] + 1]
+          // } else if (arr[i].children.length === 3) {
+          //   tempChildColIndexArr = [childColIndexArr[i] - 2, childColIndexArr[i], childColIndexArr[i] + 2]
+          // } else if (arr[i].children.length === 4) {
+          //   tempChildColIndexArr = [childColIndexArr[i] - 3, childColIndexArr[i] - 1, childColIndexArr[i] + 1, childColIndexArr[i] + 3]
+          // }
           find(arr[i].children, { rolIndex: rolIndex + 2, childColIndexArr: tempChildColIndexArr })
         }
       }
@@ -33,13 +46,13 @@ export default function TreeLight() {
     const treeDataSourceCopy = deepClone(treeDataSource)
     //起始行数： -1 + 2 = 1
     //起始列： 2
-    find(treeDataSourceCopy, { rolIndex: -1, childColIndexArr: [2] })
+    find(treeDataSourceCopy, { rolIndex: -1, childColIndexArr: [6] })
     return treeDataSourceCopy
   }
 
   //切换数据源
   let treeDataResult 
-  let treeDataSource = treeData3
+  let treeDataSource = treeData4
   treeDataResult = handleAddPositon({ treeDataSource })
 
   //打印添加position后的tree
--- src/components/light/data.js
+++ src/components/light/data.js
@@ -1,4 +1,4 @@
-//转化后的数据（一个孩子）
+//转化后的数据（1个孩子）
 const treeData1 = [
   {
     name: '申请人',
@@ -42,7 +42,7 @@ const treeData1 = [
   },
 ]
 
-//转化后的数据（两个孩子）
+//转化后的数据（2个孩子）
 const treeData2 = [
   {
     name: '申请人',
@@ -112,7 +112,7 @@ const treeData2 = [
   },
 ]
 
-//转化后的数据（三个孩子）
+//转化后的数据（3个孩子）
 const treeData3 = [
   {
     name: '申请人',
@@ -208,5 +208,127 @@ const treeData3 = [
   },
 ]
 
+//转化后的数据（4个孩子）
+const treeData4 = [
+  {
+    name: '申请人',
+    content: '商之讯',
+    belongCategory: '0',
+    status: true,
+    id: 1622771045562,
+    color: 'blue',
+    positon: {
+      rolIndex: 1,
+      colIndex: 6,
+    },
+    children: [
+      {
+        name: '审批人',
+        content: '申请人自选 不限范围 多选 依次审批',
+        belongCategory: 1622771045562,
+        status: false,
+        id: 1622771052842,
+        color: 'orange',
+        positon: {
+          rolIndex: 3,
+          colIndex: 3,
+        },
+        children: [
+          {
+            name: '抄送人',
+            content: '申请人自选',
+            belongCategory: 1622771052842,
+            status: false,
+            id: 1636424256035,
+            color: 'green',
+            positon: {
+              rolIndex: 5,
+              colIndex: 3,
+            },
+          },
+        ],
+      },
+      {
+        name: '审批人',
+        content: '申请人自选 不限范围 多选 依次审批',
+        belongCategory: 1622771045562,
+        status: false,
+        id: 1622771052842,
+        color: 'orange',
+        positon: {
+          rolIndex: 3,
+          colIndex: 5,
+        },
+        children: [
+          {
+            name: '抄送人',
+            content: '申请人自选',
+            belongCategory: 1622771052842,
+            status: false,
+            id: 1636424256035,
+            color: 'green',
+            positon: {
+              rolIndex: 5,
+              colIndex: 5,
+            },
+          },
+        ],
+      },
+      {
+        name: '审批人',
+        content: '申请人自选 不限范围 多选 依次审批',
+        belongCategory: 1622771045562,
+        status: false,
+        id: 1622771052842,
+        color: 'orange',
+        positon: {
+          rolIndex: 3,
+          colIndex: 7,
+        },
+        children: [
+          {
+            name: '抄送人',
+            content: '申请人自选',
+            belongCategory: 1622771052842,
+            status: false,
+            id: 1636424256035,
+            color: 'green',
+            positon: {
+              rolIndex: 5,
+              colIndex: 7,
+            },
+          },
+        ],
+      },
+      {
+        name: '审批人',
+        content: '申请人自选 不限范围 多选 依次审批',
+        belongCategory: 1622771045562,
+        status: false,
+        id: 1622771052842,
+        color: 'orange',
+        positon: {
+          rolIndex: 3,
+          colIndex: 9,
+        },
+        children: [
+          {
+            name: '抄送人',
+            content: '申请人自选',
+            belongCategory: 1622771052842,
+            status: false,
+            id: 1636424256035,
+            color: 'green',
+            positon: {
+              rolIndex: 5,
+              colIndex: 9,
+            },
+          },
+        ],
+      },
+    ],
+  },
+]
 
-export { treeData1, treeData2, treeData3 }
+
+export { treeData1, treeData2, treeData3, treeData4 }
