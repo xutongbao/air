7d6be68f029389f4ae70e2a3088804215eb3deca
xutongbao <1183391880@qq.com>
2021-11-15 10:26:00 +0800

    优化

--- src/components/light/TreeLine.js
+++ src/components/light/TreeLine.js
@@ -4,7 +4,15 @@ export default function TreeLine(props) {
   const { lineType } = props
   return (
     <div className="m-tree-line-wrap">
-      {JSON.stringify(lineType)}
+      {/* {JSON.stringify(lineType)} */}
+      <div className="m-tree-line-row">
+        <div className={`m-tree-line-item ${lineType.includes(1) ? 'right' : ' '} ${lineType.includes(4) ? 'bottom' : ' '}`}></div>
+        <div className={`m-tree-line-item ${lineType.includes(1) ? 'left' : ' '} ${lineType.includes(2) ? 'bottom' : ' '}`}></div>
+      </div>
+      <div className="m-tree-line-row">
+        <div className={`m-tree-line-item ${lineType.includes(3) ? 'right' : ' '} ${lineType.includes(4) ? 'top' : ' '}`}></div>
+        <div className={`m-tree-line-item ${lineType.includes(2) ? 'top' : ' '} ${lineType.includes(3) ? 'left' : ' '}` }></div>
+      </div>
     </div>
   )
 }
