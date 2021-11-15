bcc4e78e7e9462d9d628c20848db315d4a4f8272
xutongbao <1183391880@qq.com>
2021-11-15 16:15:12 +0800

    优化

--- src/components/light/treeLight/TreeCard.js
+++ src/components/light/treeLight/TreeCard.js
@@ -7,28 +7,36 @@ export default function TreeCard(props) {
     e.stopPropagation()
     props.onAddChild(item)
   }
+  const handleDelete = (e) => {
+    e.stopPropagation()
+    props.onDelete(item)
+  }
   return (
-    <div className={`m-tree-card ${color}`} id={`${item.belongCategory === '0' ? 'm-tree-root': ''}`}>
+    <div
+      className={`m-tree-card ${color}`}
+      onClick={() => props.onEdit(item)}
+      id={`${item.belongCategory === '0' ? 'm-tree-root' : ''}`}
+    >
       <div className={`m-tree-card-header ${color}`}>
         <div className="m-tree-card-header-title">{title}</div>
         {item.belongCategory !== '0' && (
           <Icon
             className="m-tree-card-header-delete"
             name="delete"
-            onClick={() => props.onDelete(item)}
+            onClick={handleDelete}
           ></Icon>
         )}
       </div>
-      <div className="m-tree-card-content" onClick={() => props.onEdit(item)}>
+      <div className="m-tree-card-content">
         <div className="m-tree-card-content-text" title={children}>
           {children}
         </div>
         <span className="m-tree-card-add-wrap">
-        <Icon
-          name="add"
-          onClick={handleAdd}
-          className="m-tree-card-add-icon"
-        ></Icon>
+          <Icon
+            name="bg-add"
+            onClick={handleAdd}
+            className="m-tree-card-add"
+          ></Icon>
         </span>
       </div>
     </div>
