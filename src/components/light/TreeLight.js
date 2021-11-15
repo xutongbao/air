8afe923833f397d0a3d24296e7fd49c22a53f07e
xutongbao <1183391880@qq.com>
2021-11-15 12:20:54 +0800

    优化

--- src/components/light/TreeLight.js
+++ src/components/light/TreeLight.js
@@ -3,234 +3,12 @@ import TreeCard from './TreeCard'
 import { deepClone } from '../../utils/tools'
 // import { treeData1, treeData2, treeData3, treeData4 } from './data'
 import TreeLine from './TreeLine'
+import useTreeLight from './useTreeLight'
 
 export default function TreeLight(props) {
   const { dataSource, onAddChild, onDelete, onEdit } = props
-
-  //统计一个棵树各层的元素的个数，并求出最大值
-  const getMaxTreeLevelNodeCount = ({ treeDataSource }) => {
-    let levelObj = {}
-    const find = (arr, level = 0) => {
-      for (let i = 0; i < arr.length; i++) {
-        arr[i].level = level
-        levelObj[level] = levelObj[level] ? levelObj[level] + 1 : 1
-        if (Array.isArray(arr[i].children) && arr[i].children.length > 0) {
-          find(arr[i].children, level + 1)
-        }
-      }
-    }
-    const treeDataSourceCopy = deepClone(treeDataSource)
-    find(treeDataSourceCopy)
-
-    let levelCountArr = []
-    for (let key in levelObj) {
-      levelCountArr.push(levelObj[key])
-    }
-    let maxLevelCount = 0
-    if (Array.isArray(levelCountArr) && levelCountArr.length > 0) {
-      maxLevelCount = Math.max.apply(null, levelCountArr)
-    }
-
-    return maxLevelCount
-  }
-
-  // const count = getMaxTreeLevelNodeCount({ treeDataSource: dataSource })
-  // console.log(count)
-
-  //添加position
-  const handleAddPosition = ({ treeDataSource }) => {
-    //rolIndex计算方式：孩子节点rolIndex = 父节点rolIndex + 2
-    //colIndex计算方式：
-    //(1)startColIndex标识起始位置，
-    //(2)如果是第一个节点，需要加上一个levelMove偏移量，这个levelMove偏移量是
-    //根据该节点的children计算出来的，和最大层节点数有关
-    //(3)如果不是第一个节点，除了和levelMove偏移量有关外，和紧邻的上一个兄弟节点的位置也有关，
-    //和紧邻的上一个兄弟节点的levelMove也有关，
-    const setPosition = (arr, { rolIndex, startColIndex, isRoot }) => {
-      for (let i = 0; i < arr.length; i++) {
-        const maxTreeLevelNodeCount = getMaxTreeLevelNodeCount({
-          treeDataSource: [arr[i]],
-        })
-        let levelMove = 0
-        if (maxTreeLevelNodeCount % 2 === 0) {
-          levelMove = ((maxTreeLevelNodeCount - 2) / 2) * 2 + 1
-        } else {
-          levelMove = ((maxTreeLevelNodeCount - 1) / 2) * 2
-        }
-        let fatherColIndex
-        if (i === 0) {
-          fatherColIndex = startColIndex + (isRoot ? levelMove : 0)
-        } else if (i > 0 && arr[i - 1].position) {
-          fatherColIndex =
-            arr[i - 1].position.colIndex +
-            2 +
-            arr[i - 1].position.levelMove +
-            levelMove
-        }
-        arr[i].position = {
-          rolIndex: rolIndex + 2,
-          colIndex: fatherColIndex,
-          levelMove,
-        }
-        if (Array.isArray(arr[i].children) && arr[i].children.length > 0) {
-          let childrenLength = arr[i].children.length
-          //奇数偶数处理方式不同
-          let startColIndex
-
-          if (childrenLength % 2 === 1) {
-            startColIndex = fatherColIndex - ((childrenLength - 1) / 2) * 2
-          } else {
-            startColIndex = fatherColIndex - ((childrenLength - 2) / 2) * 2 - 1
-          }
-          setPosition(arr[i].children, {
-            rolIndex: rolIndex + 2,
-            startColIndex,
-          })
-        }
-      }
-    }
-    const treeDataSourceCopy = deepClone(treeDataSource)
-    //起始行数： -1 + 2 = 1
-    //起始列： 2
-    //初步设置position
-    setPosition(treeDataSourceCopy, {
-      rolIndex: -1,
-      startColIndex: 2,
-      isRoot: true,
-    })
-    //找出最小的colIndex
-    //平移这个树
-
-    return treeDataSourceCopy
-  }
-
-  //添加lines
-  const handleAddLines = ({ treeDataSource }) => {
-    const setLines = (arr) => {
-      for (let i = 0; i < arr.length; i++) {
-        const rolIndex = arr[i].position.rolIndex + 1
-        arr[i].lines = []
-
-        if (Array.isArray(arr[i].children) && arr[i].children.length > 0) {
-          const fatherColIndex = arr[i].position.colIndex
-          const childrenColIndexArr = arr[i].children.map(
-            (item) => {
-              console.log(item)
-              if (item.position.colIndex) {
-                return item.position.colIndex
-              }
-            } 
-          )
-
-          for (
-            let j = arr[i].children[0].position.colIndex;
-            j < arr[i].children[arr[i].children.length - 1].position.colIndex;
-            j++
-          ) {
-            //设置不合孩子节点及父节点在同一列的lineType
-            if (!childrenColIndexArr.includes(j) && j !== fatherColIndex) {
-              arr[i].lines.push({
-                rolIndex,
-                colIndex: j,
-                lineType: [2, 4],
-              })
-            }
-            //设置父节点正下方的lineType
-            if (j === fatherColIndex) {
-              if (arr[i].children.length > 1) {
-                arr[i].lines.push({
-                  rolIndex,
-                  colIndex: j,
-                  lineType: [1, 2, 4],
-                })
-              }
-            }
-          }
-
-          //设置孩子节点正上方的lineType
-          for (let j = 0; j < arr[i].children.length; j++) {
-            const tempColIndex = arr[i].children[j].position.colIndex
-            if (tempColIndex < fatherColIndex && j === 0) {
-              arr[i].lines.push({
-                rolIndex,
-                colIndex: tempColIndex,
-                lineType: [2, 3],
-              })
-            } else if (
-              tempColIndex > fatherColIndex &&
-              j === arr[i].children.length - 1
-            ) {
-              arr[i].lines.push({
-                rolIndex,
-                colIndex: tempColIndex,
-                lineType: [4, 3],
-              })
-            } else if (
-              arr[i].children.length === 1 &&
-              fatherColIndex === tempColIndex
-            ) {
-              arr[i].lines.push({
-                rolIndex,
-                colIndex: tempColIndex,
-                lineType: [1, 3],
-              })
-            } else if (
-              arr[i].children.length > 1 &&
-              fatherColIndex === tempColIndex
-            ) {
-              arr[i].lines.push({
-                rolIndex,
-                colIndex: tempColIndex,
-                lineType: [1, 2, 3, 4],
-              })
-            } else if (
-              arr[i].children.length > 1 &&
-              fatherColIndex > tempColIndex &&
-              j !== 0
-            ) {
-              arr[i].lines.push({
-                rolIndex,
-                colIndex: tempColIndex,
-                lineType: [2, 3, 4],
-              })
-            } else if (
-              arr[i].children.length > 1 &&
-              fatherColIndex < tempColIndex &&
-              j !== arr[i].children.length - 1
-            ) {
-              arr[i].lines.push({
-                rolIndex,
-                colIndex: tempColIndex,
-                lineType: [2, 3, 4],
-              })
-            }
-          }
-
-          setLines(arr[i].children)
-        }
-      }
-    }
-    const treeDataSourceCopy = deepClone(treeDataSource)
-    setLines(treeDataSourceCopy)
-    return treeDataSourceCopy
-  }
-
-  //切换数据源
-  let treeDataResult
-  //let treeDataSource = treeData4
-  let treeDataSource = dataSource
-  if (Array.isArray(treeDataSource) && treeDataSource.length > 0) {
-    treeDataResult = handleAddPosition({ treeDataSource })
-    console.log(treeDataResult)
-    const hasLines = handleAddLines({ treeDataSource: treeDataResult })
-    console.log(hasLines)
-    treeDataResult = hasLines
-  }
-
-  //打印添加position后的tree
-  // console.log(treeDataResult)
-  //切换真正用于渲染的treeData
-  let treeData = treeDataResult ? treeDataResult : treeDataSource
+  //添加position和lines
+  const { treeData } = useTreeLight({ dataSource })
 
   //查找行列值和position值一致的元素
   const findTreeNode = ({ treeData, position }) => {
@@ -279,7 +57,7 @@ export default function TreeLight(props) {
   }
 
   //根据树结构判断是否应该渲染TreeCard组件
-  const renderTreeCard = ({ rolIndex, colIndex }) => {
+  const renderTreeCol = ({ rolIndex, colIndex }) => {
     const treeNode = findTreeNode({
       treeData,
       position: { rolIndex, colIndex },
@@ -307,8 +85,8 @@ export default function TreeLight(props) {
     )
   }
 
-  //基础网格
-  const getDom = () => {
+  //渲染dom
+  const renderDom = () => {
     const dataArr = []
     //20行， 10列
     for (let i = 0; i < 20; i++) {
@@ -331,7 +109,7 @@ export default function TreeLight(props) {
                 key={`${rolIndex}-${colIndex}`}
                 className={`m-tree-col ${isDev ? 'active' : ''}`}
               >
-                {renderTreeCard({ rolIndex, colIndex })}
+                {renderTreeCol({ rolIndex, colIndex })}
               </div>
             ))}
           </div>
@@ -342,7 +120,7 @@ export default function TreeLight(props) {
 
   return (
     <div className="m-tree-wrap">
-      <div className="m-tree-row">{getDom()}</div>
+      <div className="m-tree-row">{renderDom()}</div>
     </div>
   )
 }
---
+++ src/components/light/useTreeLight.js
@@ -0,0 +1,221 @@
+import { deepClone } from '../../utils/tools'
+
+export default function useTreeLight({ dataSource }) {
+  //统计一个棵树各层的元素的个数，并求出最大值
+  const getMaxTreeLevelNodeCount = ({ treeDataSource }) => {
+    let levelObj = {}
+    const find = (arr, level = 0) => {
+      for (let i = 0; i < arr.length; i++) {
+        arr[i].level = level
+        levelObj[level] = levelObj[level] ? levelObj[level] + 1 : 1
+        if (Array.isArray(arr[i].children) && arr[i].children.length > 0) {
+          find(arr[i].children, level + 1)
+        }
+      }
+    }
+    const treeDataSourceCopy = deepClone(treeDataSource)
+    find(treeDataSourceCopy)
+
+    let levelCountArr = []
+    for (let key in levelObj) {
+      levelCountArr.push(levelObj[key])
+    }
+    let maxLevelCount = 0
+    if (Array.isArray(levelCountArr) && levelCountArr.length > 0) {
+      maxLevelCount = Math.max.apply(null, levelCountArr)
+    }
+
+    return maxLevelCount
+  }
+
+  // const count = getMaxTreeLevelNodeCount({ treeDataSource: dataSource })
+  // console.log(count)
+
+  //添加position
+  const handleAddPosition = ({ treeDataSource }) => {
+    //rolIndex计算方式：孩子节点rolIndex = 父节点rolIndex + 2
+    //colIndex计算方式：
+    //(1)startColIndex标识起始位置，
+    //(2)如果是第一个节点，需要加上一个levelMove偏移量，这个levelMove偏移量是
+    //根据该节点的children计算出来的，和最大层节点数有关
+    //(3)如果不是第一个节点，除了和levelMove偏移量有关外，和紧邻的上一个兄弟节点的位置也有关，
+    //和紧邻的上一个兄弟节点的levelMove也有关，
+    const setPosition = (arr, { rolIndex, startColIndex, isRoot }) => {
+      for (let i = 0; i < arr.length; i++) {
+        const maxTreeLevelNodeCount = getMaxTreeLevelNodeCount({
+          treeDataSource: [arr[i]],
+        })
+        let levelMove = 0
+        if (maxTreeLevelNodeCount % 2 === 0) {
+          levelMove = ((maxTreeLevelNodeCount - 2) / 2) * 2 + 1
+        } else {
+          levelMove = ((maxTreeLevelNodeCount - 1) / 2) * 2
+        }
+        let fatherColIndex
+        if (i === 0) {
+          fatherColIndex = startColIndex + (isRoot ? levelMove : 0)
+        } else if (i > 0 && arr[i - 1].position) {
+          fatherColIndex =
+            arr[i - 1].position.colIndex +
+            2 +
+            arr[i - 1].position.levelMove +
+            levelMove
+        }
+        arr[i].position = {
+          rolIndex: rolIndex + 2,
+          colIndex: fatherColIndex,
+          levelMove,
+        }
+        if (Array.isArray(arr[i].children) && arr[i].children.length > 0) {
+          let childrenLength = arr[i].children.length
+          //奇数偶数处理方式不同
+          let startColIndex
+
+          if (childrenLength % 2 === 1) {
+            startColIndex = fatherColIndex - ((childrenLength - 1) / 2) * 2
+          } else {
+            startColIndex = fatherColIndex - ((childrenLength - 2) / 2) * 2 - 1
+          }
+          setPosition(arr[i].children, {
+            rolIndex: rolIndex + 2,
+            startColIndex,
+          })
+        }
+      }
+    }
+    const treeDataSourceCopy = deepClone(treeDataSource)
+    //起始行数： -1 + 2 = 1
+    //起始列： 2
+    //初步设置position
+    setPosition(treeDataSourceCopy, {
+      rolIndex: -1,
+      startColIndex: 2,
+      isRoot: true,
+    })
+    //找出最小的colIndex
+    //平移这个树
+
+    return treeDataSourceCopy
+  }
+
+  //添加lines
+  const handleAddLines = ({ treeDataSource }) => {
+    const setLines = (arr) => {
+      for (let i = 0; i < arr.length; i++) {
+        const rolIndex = arr[i].position.rolIndex + 1
+        arr[i].lines = []
+
+        if (Array.isArray(arr[i].children) && arr[i].children.length > 0) {
+          const fatherColIndex = arr[i].position.colIndex
+          const childrenColIndexArr = arr[i].children.map(
+            (item) => item.position.colIndex
+          )
+
+          for (
+            let j = arr[i].children[0].position.colIndex;
+            j < arr[i].children[arr[i].children.length - 1].position.colIndex;
+            j++
+          ) {
+            //设置不合孩子节点及父节点在同一列的lineType
+            if (!childrenColIndexArr.includes(j) && j !== fatherColIndex) {
+              arr[i].lines.push({
+                rolIndex,
+                colIndex: j,
+                lineType: [2, 4],
+              })
+            }
+            //设置父节点正下方的lineType
+            if (j === fatherColIndex) {
+              if (arr[i].children.length > 1) {
+                arr[i].lines.push({
+                  rolIndex,
+                  colIndex: j,
+                  lineType: [1, 2, 4],
+                })
+              }
+            }
+          }
+
+          //设置孩子节点正上方的lineType
+          for (let j = 0; j < arr[i].children.length; j++) {
+            const tempColIndex = arr[i].children[j].position.colIndex
+            if (tempColIndex < fatherColIndex && j === 0) {
+              arr[i].lines.push({
+                rolIndex,
+                colIndex: tempColIndex,
+                lineType: [2, 3],
+              })
+            } else if (
+              tempColIndex > fatherColIndex &&
+              j === arr[i].children.length - 1
+            ) {
+              arr[i].lines.push({
+                rolIndex,
+                colIndex: tempColIndex,
+                lineType: [4, 3],
+              })
+            } else if (
+              arr[i].children.length === 1 &&
+              fatherColIndex === tempColIndex
+            ) {
+              arr[i].lines.push({
+                rolIndex,
+                colIndex: tempColIndex,
+                lineType: [1, 3],
+              })
+            } else if (
+              arr[i].children.length > 1 &&
+              fatherColIndex === tempColIndex
+            ) {
+              arr[i].lines.push({
+                rolIndex,
+                colIndex: tempColIndex,
+                lineType: [1, 2, 3, 4],
+              })
+            } else if (
+              arr[i].children.length > 1 &&
+              fatherColIndex > tempColIndex &&
+              j !== 0
+            ) {
+              arr[i].lines.push({
+                rolIndex,
+                colIndex: tempColIndex,
+                lineType: [2, 3, 4],
+              })
+            } else if (
+              arr[i].children.length > 1 &&
+              fatherColIndex < tempColIndex &&
+              j !== arr[i].children.length - 1
+            ) {
+              arr[i].lines.push({
+                rolIndex,
+                colIndex: tempColIndex,
+                lineType: [2, 3, 4],
+              })
+            }
+          }
+
+          setLines(arr[i].children)
+        }
+      }
+    }
+    const treeDataSourceCopy = deepClone(treeDataSource)
+    setLines(treeDataSourceCopy)
+    return treeDataSourceCopy
+  }
+
+  //切换数据源
+  let treeDataResult
+  let treeDataSource = dataSource
+  if (Array.isArray(treeDataSource) && treeDataSource.length > 0) {
+    treeDataResult = handleAddPosition({ treeDataSource })
+    const hasLines = handleAddLines({ treeDataSource: treeDataResult })
+    treeDataResult = hasLines
+  }
+
+  //切换真正用于渲染的treeData
+  let treeData = treeDataResult ? treeDataResult : treeDataSource
+  return {
+    treeData,
+  }
+}
