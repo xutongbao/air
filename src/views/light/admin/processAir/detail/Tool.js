baf2d61cd5274be63823d90176ae46bee6ee420b
xutongbao <1183391880@qq.com>
2021-11-15 18:46:32 +0800

    优化

--- src/views/light/admin/processAir/detail/Tool.js
+++ src/views/light/admin/processAir/detail/Tool.js
@@ -5,7 +5,7 @@ import { Icon } from '../../../../../components/light'
 function Tool(props) {
   const { onResetTreeToCenter, onScale } = props
   function formatter(value) {
-    return `${value}%`;
+    return `${value}%`
   }
 
   return (
@@ -21,7 +21,32 @@ function Tool(props) {
             复位
           </Button>
           <span className="m-space">调整大小：</span>
-          <Slider defaultValue={100} min={50} max={100} onAfterChange={onScale} tipFormatter={formatter} tooltipVisible className="m-space m-tree-slider" getTooltipPopupContainer={() => document.getElementById('m-content-wrap')}/>
+          <Slider
+            defaultValue={100}
+            min={50}
+            max={100}
+            onAfterChange={onScale}
+            tipFormatter={formatter}
+            tooltipVisible
+            className="m-space m-tree-slider"
+            getTooltipPopupContainer={() =>
+              document.getElementById('m-content-wrap')
+            }
+          />
+        </div>
+        <div className="m-content-tool-row">
+          <Button
+            className="m-space"
+            type="primary"
+            onClick={onResetTreeToCenter}
+          >
+            <Icon name="submit" className="m-tool-btn-icon"></Icon>
+            保存
+          </Button>
+          <Button className="m-space" onClick={onResetTreeToCenter}>
+            <Icon name="reset" className="m-tool-btn-icon"></Icon>
+            重置
+          </Button>
         </div>
       </div>
     </>
