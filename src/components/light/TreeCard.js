8d41bef952d8dcfd530c09aa68915cf95d58e6b2
xutongbao <1183391880@qq.com>
2021-11-15 12:49:54 +0800

    优化

--- src/components/light/TreeCard.js
+++ src/components/light/TreeCard.js
@@ -11,11 +11,13 @@ export default function TreeCard(props) {
     <div className={`m-tree-card ${color}`}>
       <div className={`m-tree-card-header ${color}`}>
         <div className="m-tree-card-header-title">{title}</div>
-        <Icon
-          className="m-tree-card-header-delete"
-          name="delete"
-          onClick={() => props.onDelete(item)}
-        ></Icon>
+        {item.belongCategory !== '0' && (
+          <Icon
+            className="m-tree-card-header-delete"
+            name="delete"
+            onClick={() => props.onDelete(item)}
+          ></Icon>
+        )}
       </div>
       <div className="m-tree-card-content" onClick={() => props.onEdit(item)}>
         <div className="m-tree-card-content-text" title={children}>
